b0d083512777799724eeb8ff4ab946c8bd552178
ironiSharp <77969206+ironiSharp@users.noreply.github.com>
2021-01-25 12:11:43 +0000

    First Commit
    Added all the code

---
+++ externalKeystrokes/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace externalKeystrokes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnW = new System.Windows.Forms.Button();
+            this.btnS = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.LMButton = new System.Windows.Forms.Button();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.RMButton = new System.Windows.Forms.Button();
+            this.sprintTimer = new System.Windows.Forms.Timer(this.components);
+            this.toggleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnW
+            // 
+            this.btnW.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btnW.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnW.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.btnW.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.btnW.Location = new System.Drawing.Point(95, 431);
+            this.btnW.Name = "btnW";
+            this.btnW.Size = new System.Drawing.Size(75, 71);
+            this.btnW.TabIndex = 0;
+            this.btnW.Text = "W";
+            this.btnW.UseVisualStyleBackColor = false;
+            // 
+            // btnS
+            // 
+            this.btnS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btnS.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnS.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.btnS.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.btnS.Location = new System.Drawing.Point(95, 508);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(75, 72);
+            this.btnS.TabIndex = 1;
+            this.btnS.Text = "S";
+            this.btnS.UseVisualStyleBackColor = false;
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btnD.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnD.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.btnD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.btnD.Location = new System.Drawing.Point(176, 508);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(75, 72);
+            this.btnD.TabIndex = 2;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = false;
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.btnA.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnA.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.btnA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.btnA.Location = new System.Drawing.Point(14, 508);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(75, 72);
+            this.btnA.TabIndex = 3;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = false;
+            // 
+            // LMButton
+            // 
+            this.LMButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.LMButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.LMButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LMButton.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.LMButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.LMButton.Location = new System.Drawing.Point(14, 586);
+            this.LMButton.Name = "LMButton";
+            this.LMButton.Size = new System.Drawing.Size(116, 72);
+            this.LMButton.TabIndex = 4;
+            this.LMButton.Text = "LMB";
+            this.LMButton.UseVisualStyleBackColor = false;
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Interval = 10;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // RMButton
+            // 
+            this.RMButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.RMButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.RMButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RMButton.Font = new System.Drawing.Font("Segoe UI Light", 16F);
+            this.RMButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.RMButton.Location = new System.Drawing.Point(135, 586);
+            this.RMButton.Name = "RMButton";
+            this.RMButton.Size = new System.Drawing.Size(116, 72);
+            this.RMButton.TabIndex = 5;
+            this.RMButton.Text = "RMB";
+            this.RMButton.UseVisualStyleBackColor = false;
+            // 
+            // sprintTimer
+            // 
+            this.sprintTimer.Interval = 20;
+            this.sprintTimer.Tick += new System.EventHandler(this.sprintTimer_Tick);
+            // 
+            // toggleLabel
+            // 
+            this.toggleLabel.AutoSize = true;
+            this.toggleLabel.Font = new System.Drawing.Font("Segoe UI Light", 24F);
+            this.toggleLabel.ForeColor = System.Drawing.Color.White;
+            this.toggleLabel.Location = new System.Drawing.Point(12, 33);
+            this.toggleLabel.Name = "toggleLabel";
+            this.toggleLabel.Size = new System.Drawing.Size(312, 45);
+            this.toggleLabel.TabIndex = 6;
+            this.toggleLabel.Text = "[(Toggle Sprint: False)]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 676);
+            this.Controls.Add(this.toggleLabel);
+            this.Controls.Add(this.RMButton);
+            this.Controls.Add(this.LMButton);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnS);
+            this.Controls.Add(this.btnW);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keystrokes/Toggle Sprint";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnW;
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button LMButton;
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Button RMButton;
+        private System.Windows.Forms.Timer sprintTimer;
+        private System.Windows.Forms.Label toggleLabel;
+    }
+}
+
---
+++ externalKeystrokes/Form1.cs
@@ -0,0 +1,145 @@
+﻿using externalKeystrokes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Drawing2D;
+
+namespace externalKeystrokes
+{
+    public partial class Form1 : Form
+    {
+        public const string WINDOW_NAME = "Minecraft";
+
+        public const int VK_LCONTROL = 0xA2;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            this.BackColor = Color.Wheat;
+            this.TransparencyKey = Color.Wheat;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            int initialStyle = imports.GetWindowLong(this.Handle, -20);
+            imports.SetWindowLong(this.Handle, -20, initialStyle | 0x8000 | 0x20);
+
+            imports.GetWindowRect(imports.handle, out imports.rect);
+            this.Size = new Size(imports.rect.right - imports.rect.left, imports.rect.bottom - imports.rect.top);
+            this.Left = imports.rect.left;
+
+            this.Top = imports.rect.top;
+
+            mainTimer.Start();
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            int chars = 256;
+            StringBuilder buff = new StringBuilder(chars);
+
+            IntPtr handle = imports.GetForegroundWindow();
+
+            if (imports.GetWindowText(handle, buff, chars) > 0)
+            {
+                string windowName = buff.ToString(); // this extra variable is here so you can check if contains
+
+                if (windowName.Contains("Minecraft"))
+                {
+                    this.Opacity = 100;
+                    sprintTimer.Start();
+                }
+                else
+                {
+                    this.Opacity = 0;
+                    sprintTimer.Stop();
+                }
+            }
+
+            this.BringToFront();
+            this.TopMost = true;
+
+            if (imports.GetAsyncKeyState(Keys.W) < 0)
+            {
+                btnW.BackColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                btnW.BackColor = Color.FromArgb(30, 30, 30);
+            }
+
+            if (imports.GetAsyncKeyState(Keys.S) < 0)
+            {
+                btnS.BackColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                btnS.BackColor = Color.FromArgb(30, 30, 30);
+            }
+
+            if (imports.GetAsyncKeyState(Keys.A) < 0)
+            {
+                btnA.BackColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                btnA.BackColor = Color.FromArgb(30, 30, 30);
+            }
+
+            if (imports.GetAsyncKeyState(Keys.D) < 0)
+            {
+                btnD.BackColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                btnD.BackColor = Color.FromArgb(30, 30, 30);
+            }
+
+            if (imports.GetAsyncKeyState(Keys.LButton) < 0)
+            {
+                LMButton.BackColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                LMButton.BackColor = Color.FromArgb(30, 30, 30);
+            }
+
+            if (imports.GetAsyncKeyState(Keys.RButton) < 0)
+            {
+                RMButton.BackColor = Color.FromArgb(60, 60, 60);
+            }
+            else
+            {
+                RMButton.BackColor = Color.FromArgb(30, 30, 30);
+            }
+
+        }
+
+        private void sprintTimer_Tick(object sender, EventArgs e)
+        {
+            if (imports.GetAsyncKeyState(Keys.W) < 0)
+            {
+                imports.keybd_event(VK_LCONTROL, 0, 0, 0);
+                toggleLabel.Text = @"[(Toggle Sprint: Sprinting)]";
+            }
+            else
+            {
+                imports.keybd_event(VK_LCONTROL, 0, KEYEVENTF_KEYUP, 0);
+                toggleLabel.Text = @"[(Toggle Sprint: Not Sprinting)]";
+            }
+        }
+    }
+}
